0d95e2b7ce840e41de93036009fbf51489b846eb
ukiyoyo <13175394+ukiyoyo@users.noreply.github.com>
2020-09-03 00:34:43 +0900

    Add test case

--- src/graph/minimum_spanning_tree.rs
+++ src/graph/minimum_spanning_tree.rs
@@ -59,4 +59,39 @@ mod test {
         assert_eq!(mst.selected, vec![(0, 1, 1), (2, 3, 1), (1, 2, 2),]);
         assert_eq!(mst.total_cost, 4);
     }
+
+    /// https://atcoder.jp/contests/abc065/tasks/arc076_b
+    #[test]
+    fn solve_abc065_d() {
+        let n = 6;
+        let xy = vec![(8, 3), (4, 9), (12, 19), (18, 1), (13, 5), (7, 6)];
+
+        let mut xyi = xy
+            .into_iter()
+            .enumerate()
+            .map(|(i, (x, y))| (x, y, i))
+            .collect::<Vec<(i64, i64, usize)>>();
+
+        let mut edges = vec![];
+        xyi.sort();
+        for i in 0..(n - 1) {
+            let dist = std::cmp::min(
+                (xyi[i].0 - xyi[i + 1].0).abs(),
+                (xyi[i].1 - xyi[i + 1].1).abs(),
+            );
+            edges.push((xyi[i].2, xyi[i + 1].2, dist));
+        }
+        xyi.sort_by_cached_key(|&(_, y, _)| y);
+        for i in 0..(n - 1) {
+            let dist = std::cmp::min(
+                (xyi[i].0 - xyi[i + 1].0).abs(),
+                (xyi[i].1 - xyi[i + 1].1).abs(),
+            );
+            edges.push((xyi[i].2, xyi[i + 1].2, dist));
+        }
+
+        let mut mst = MinimumSpanningTree::new(n, &edges);
+        mst.kruskal();
+        assert_eq!(mst.total_cost, 8);
+    }
 }
